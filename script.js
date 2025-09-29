b439911893e5f9f5f9a8db635cc2f090dd9a3778
Your GitHub Name <lujain.aljehani@outlook.com>
2025-09-29 11:51:09 +0300

    TODO2

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
     //TODO1
     document.getElementById("t1-msg").textContent = "Hello, World!";
 
+    //TODO2
+    document.getElementById("t2-btn").addEventListener("click", function () {
+        document.getElementById("t2-status").textContent = "You clicked the button!";
+    });
 
 });
