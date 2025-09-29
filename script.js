0a5ff835f590afeb0b9be1981574f31e2e124f93
Your GitHub Name <lujain.aljehani@outlook.com>
2025-09-29 12:02:19 +0300

    TODO3 updated 'data.quote'

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const res = await fetch("https://dummyjson.com/quotes/random");
             if (!res.ok) throw new Error("HTTP " + res.status);
             const data = await res.json();
-            document.getElementById("t3-quote").textContent = data.content || data.quote || "No quote";
+            document.getElementById("t3-quote").textContent = data.quote || "No quote";
             document.getElementById("t3-author").textContent = data.author || "Unknown";
         } catch {
             document.getElementById("t3-quote").textContent = "Unable to load a quote.";
