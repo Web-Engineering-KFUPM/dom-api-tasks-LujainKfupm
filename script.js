505cc734237fa2615a2c8784779aa6c54f80ee12
Your GitHub Name <lujain.aljehani@outlook.com>
2025-09-29 12:10:21 +0300

    TODO4

--- script.js
+++ script.js
@@ -23,6 +23,21 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 
-
+//TODO4
+    const key = "392405218a56e46f9ae0650d6d069134";
+    document.getElementById("t4-loadWx").addEventListener("click", async function () {
+        try {
+            const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Dammam&appid=${key}&units=metric`);
+            if (!res.ok) throw new Error("HTTP " + res.status);
+            const data = await res.json();
+            document.getElementById("t4-temp").textContent = data.main.temp + " °C";
+            document.getElementById("t4-hum").textContent = data.main.humidity + " %";
+            document.getElementById("t4-wind").textContent = data.wind.speed + " m/s";
+        } catch {
+            document.getElementById("t4-temp").textContent = "Unable load weather.";
+            document.getElementById("t4-hum").textContent = "";
+            document.getElementById("t4-wind").textContent = "";
+        }
+    });
 
 });
